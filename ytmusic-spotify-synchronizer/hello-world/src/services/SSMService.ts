edfec1907b71e2538c6d4a01ed08364eb0a20c93
Justin Stendara <Justin.stendara@coxautoinc.com>
2025-03-20 10:16:29 -0500

    VaultService works

--- ytmusic-spotify-synchronizer/hello-world/src/services/SSMService.ts
+++ ytmusic-spotify-synchronizer/hello-world/src/services/SSMService.ts
@@ -1,33 +1,47 @@
-import {SSMClient} from "@aws-sdk/client-ssm";
+import {Client} from "@litehex/node-vault";
 
-export const getSSMParameter = async (parameterName: string): Promise<string> => {
-
-    return "";
-}
-
-export const setSSMParamter = async (paramterName: string, value: string): Promise<void> => {
-
-    return
-}
-
-export class SSMService {
-    private ssmClient: SSMClient;
+export class HCPVaultService {
+    private vaultClient: Client;
 
     constructor() {
-
+        this.initializeClient();
     }
 
-    public initializeClient = (region: string = "us-east-1") => {
-        this.ssmClient = new SSMClient({
-            region: region
+    public initializeClient = () => {
+        this.vaultClient = new Client({
+            apiVersion: "v1",
+            endpoint: "http://100.82.133.11:8200",
+            token: "hvs.JPUjwVoVsjH3mrQhMDfnLKfe"
         });
     }
 
-    public getSSMParamter = (name: string): Promise<string> => {
-        return Promise.resolve("Yay!");
+    public getParameter = async (name: string): Promise<Record<string, string> | undefined> => {
+        const res = await this.vaultClient.kv2.read({
+            mountPath: "kv/",
+            path: `/music-synchronizer/${name}`
+        });
+
+        return res?.data?.data?.data;
     }
 
-    public setSSMParamter = (name: string, value: string): Promise<void> => {
-        return Promise.resolve();
+    public setParameter = async (name: string, data: Record<string, any>): Promise<void> => {
+        let parameterData:Record<string, any> = {data: data};
+        let currData = await this.getParameter(name);
+        if (currData) {
+            parameterData = {data:{...currData, ...parameterData.data}}
+        }
+
+        let result: Response = await fetch(`http://100.82.133.11:8200/v1/kv/data/music-synchronizer/${name}`, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${this.vaultClient.token}`
+            },
+            body: JSON.stringify(parameterData)
+        });
+
+        if (result.status !== 200) {
+            throw new Error(`Unexpected response code while setting parameter: ${result.status}`)
+        }
+        return;
     }
 }
