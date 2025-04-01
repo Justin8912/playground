62f563517e4fcb6cd00bb6122009b63604ccc77e
Justin Stendara <95107100+Justin8912@users.noreply.github.com>
2025-04-01 17:14:52 -0500

    Adding the name normalizer (#2)
    * Adding the name normalizer
    
    * Small updates
    
    * Adding check on the tvshowid

---
+++ name-normalizer/index.js
@@ -0,0 +1,71 @@
+import fs from "fs";
+import { TvService } from "./tmdb/api.js";
+import inquirer from "inquirer";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const tvDirectoryPath = process.env.base_tvshow_path
+
+const sanitizeFilename = (newTitle) => {
+    return newTitle.replace(/[:\/\\]/g, '-');
+} 
+
+const main = async (tvShow, year, dryRun) => {
+    const baseTvShowPath = tvDirectoryPath + tvShow
+    const seasons = fs.readdirSync(baseTvShowPath);
+    const tvService = new TvService(process.env.tvService_accessKey);
+    await tvService.setTvSeries(tvShow, year);
+
+    for (const season of seasons) {
+        const seasonPath = `${baseTvShowPath}\\${season}`
+        const episodes = fs.readdirSync(seasonPath);
+        const episodeNames = await tvService.getTvSeasonEpisodes(season.split(" ")[1])
+        for (const episode of episodes) {
+            const seFormatPattern = /s\d{2}e\d{2}/;
+            const seFormat = (episode.toLowerCase().match(seFormatPattern)).toString();
+
+            const episodeNumberPattern = /(?<=e)\d{2}/
+            const episodeNumber = parseInt((seFormat.match(episodeNumberPattern)).toString());
+
+            const fileExtensionPattern = /(?<=\.)[^.]+$/
+            const fileExtension = (episode.match(fileExtensionPattern)).toString();
+
+            const newTitle = `${tvShow} - ${seFormat} - ${episodeNames[episodeNumber-1]}.${fileExtension}`
+            const sanitizedNewTitle = sanitizeFilename(newTitle);
+
+            if (episode !== sanitizedNewTitle) {
+                if (dryRun) {
+                    console.log(`${episode} => ${sanitizedNewTitle}`)
+                } else {
+                    fs.renameSync(`${seasonPath}\\${episode}`, `${seasonPath}\\${sanitizedNewTitle}`)
+                }
+            }
+        }
+    }
+}
+
+const questions = [
+    {
+        type: 'list',
+        name: 'tvShow',
+        message: 'Choose a tv show whose names you\'d like to format:',
+        choices: fs.readdirSync(tvDirectoryPath),
+    },
+    {
+        type: 'number',
+        name: 'year',
+        message: 'The year that the tv show first premiered'
+    },
+    {
+        type: 'confirm',
+        name: 'dryRun',
+        message: 'Would you like to run as a dry run?',
+        default: false 
+    }
+];
+
+inquirer.prompt(questions).then((answers) => {
+    console.log('You selected: ', answers);
+    main(answers.tvShow, answers.year, answers.dryRun)
+});
---
+++ name-normalizer/tmdb/api.js
@@ -0,0 +1,52 @@
+export class TvService {
+    accessToken;
+    tvShowId;
+
+    constructor(accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    async setTvSeries(tvshowName, year) {
+        const baseUrl = 'https://api.themoviedb.org/3/search/tv';
+        const params = new URLSearchParams({
+            query: tvshowName,
+            include_adult: 'false',
+            language: 'en-US',
+            page: '1',
+            ...(year && {year})
+        });
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${this.accessToken}`
+            }
+        };
+
+        let res = await fetch(`${baseUrl}?${params.toString()}`, options);
+        res = await res.json();
+        this.tvShowId = res.results[0].id
+    }
+
+    async getTvSeasonEpisodes(seasonNumber) {
+        if (!this.tvShowId) {
+            console.log("You must set the tvshow before trying to find episodes.");
+            return;
+        }
+        const url = `https://api.themoviedb.org/3/tv/${this.tvShowId}/season/${seasonNumber}?language=en-US`;
+        const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${this.accessToken}`
+        }
+        };
+    
+        let res = await fetch(url, options)
+
+        res = await res.json();
+        let result = []
+        res.episodes.map(episode => {result[episode.episode_number-1]=episode.name})
+        return result
+    }
+}
