8fbcafd235026836378c2be6d472c2d8c56c3d53
Justin Stendara <Justin.stendara@coxautoinc.com>
2025-07-26 16:21:30 -0500

    Update the spotifyClient

---
+++ music-synchronizer/backend/src/Services/SpotifyClient.ts
@@ -0,0 +1,62 @@
+import { HCPVaultService } from "./VaultService.js";
+import { AccessToken } from "@spotify/web-api-ts-sdk";
+
+export class SpotifyClient {
+    private vaultService: HCPVaultService;
+    private userId: string;
+    
+    constructor(
+        vaultService: HCPVaultService, 
+        userId: string
+    ) {
+        this.vaultService = vaultService;
+        this.userId = userId;
+    }
+
+    refreshAccessToken = async (): Promise<AccessToken> => {
+        console.log("Refreshing spotify access token for user");
+
+        const { client_id: clientId, client_secret: clientSecret } = await this.vaultService.getServerSpotifyCredentials();
+        const {refresh_token} = await this.vaultService.getUserSpotifyCredentials(
+            this.userId
+        );
+
+        const url = "https://accounts.spotify.com/api/token";
+        const payload = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + btoa(`${clientId}:${clientSecret}`)
+            },
+            body: new URLSearchParams({
+                grant_type: 'refresh_token',
+                refresh_token: refresh_token,
+                client_id: clientId
+            }),
+        }
+        const body = await fetch(url, payload);
+        const response = await body.json();
+
+        const accessToken: AccessToken = {
+            access_token: response.access_token,
+            token_type: response.token_type,
+            expires_in: response.expires_in,
+            refresh_token: refresh_token
+        }
+
+        await this.vaultService.setUserSpotifyCredentials(
+            this.userId, accessToken
+        );
+
+        return accessToken;
+    }
+}
+
+export const SpotifyClientFactory = async (
+    vaultService: HCPVaultService, 
+    userId: string
+) => {
+    const spotifyClient = new SpotifyClient(vaultService, userId);
+    await spotifyClient.refreshAccessToken();
+    return spotifyClient;   
+}
---
+++ music-synchronizer/backend/src/Services/SpotifyService.ts
@@ -0,0 +1,6 @@
+export class SpotifyService {
+    
+    constructor() {
+
+    }
+}
