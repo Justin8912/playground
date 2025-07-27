10a9ae4d47ce3a695616b0c8e18c49b025e5ef9b
Justin Stendara <Justin.stendara@coxautoinc.com>
2025-07-27 01:14:16 -0500

    Stubbing out methods

--- music-synchronizer/backend/src/Services/SpotifyService.ts
+++ music-synchronizer/backend/src/Services/SpotifyService.ts
@@ -11,8 +11,25 @@ export class SpotifyService {
     }
 
     initialize = async () => {
-
+        try {
+            await this.spotifyClient.refreshAccessToken();
+            console.info("Spotify Service initialized successfully.");
+        } catch (err) {
+            throw new Error("Failed to initialize Spotify Service", { cause: err });
+        }
     }
+
+    public getPlaylistIds = async () => {
+        
+    }
+
+    public addSongsToPlaylist = async () => {}
+
+    private addSongToPlaylist = async () => {}
+
+    public getPlaylistByName = async (name: string) => {}
+
+    public removeSongFromPlaylist = async () => {}
 }
 
 export const getSpotifyService = async (
