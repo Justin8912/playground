64d160be1819f43f1df440e13622ac05cf7bb737
justin <jnstendara@gmail.com>
2025-09-02 18:02:29 -0500

    Send movie

---
+++ send-movie/constants.js
@@ -0,0 +1,7 @@
+export const HOME_SERVER = {
+    ip: "192.168.1.93",
+    user: "jnste",
+    directory: "/home/jnste/storage/media/movies"
+}
+
+export const BASE_MOVIE_DIRECTORY_PATH = "C:\\Users\\jnste\\OneDrive\\Pictures\\Movies\\"
