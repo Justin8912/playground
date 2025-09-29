2d659b4a9224157f317f0310c46b35612227f2ac
Justin Stendara <Justin.stendara@coxautoinc.com>
2025-09-29 13:25:37 -0500

    Frontend is functional

--- music-synchronizer/frontend/src/music-synchronizer.ts
+++ music-synchronizer/frontend/src/music-synchronizer.ts
@@ -1,9 +1,17 @@
 import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement, property, state} from 'lit/decorators.js';
 import "./custom-components/form-view.js";
 import "./custom-components/song-comparison-view.js";
-import {getSynchronizationProposal, synchronizePlaylist, updateSynchronizationProposal} from "./controller/controller";
-import {SynchronizationProposal} from "./model/ControllerTypes";
+import "./custom-components/waiting-view.js";
+import "./custom-components/failed-uploads-view.js";
+import {getSynchronizationProposal, synchronizePlaylist} from "./controller/controller";
+import {ProposedChangesRequestDetails, Song, SynchronizationProposal} from "./model/ControllerTypes";
+import {proposedChangesId, requestDetails} from "./util/context";
+import { provide } from '@lit/context';
+
+
+// Page type for explicit view switching
+export type Page = "form" | "comparison" | "results";
 
 @customElement('music-synchronizer')
 export class MusicSynchronizer extends LitElement {
@@ -17,7 +25,24 @@ export class MusicSynchronizer extends LitElement {
   `;
 
   @property()
+  @state()
   synchronizationProposal: SynchronizationProposal = {} as SynchronizationProposal;
+  @property()
+  errorMessage: string = "";
+  @state()
+  isLoading: boolean = false;
+  // @ts-ignore
+  @provide({context: requestDetails})
+  @state()
+  requestDetails?: ProposedChangesRequestDetails;
+  // @ts-ignore
+  @provide({context: proposedChangesId})
+  @state()
+  proposedChangesId?: string;
+  @state()
+  currentPage: Page = "form";
+  @state()
+  failedSongs: Song[] = [];
 
   renderFormView() {
     return html`
@@ -32,100 +57,134 @@ export class MusicSynchronizer extends LitElement {
     <div>
         <song-comparison-view 
           .synchronizationProposal=${proposal}
-          @remove-song-mapping=${this.handleRemoveSongMapping}
-          @update-song-mapping=${this.handleReplaceSong}
+          @update-proposal=${this.handleProposalUpdate}
         ></song-comparison-view>
-      <button @click=${this.synchronizePlaylist}>Submit changes</button>
+        <button @click=${this.synchronizePlaylist}>Submit changes</button>
     </div>`
   }
-  override render() {
-    const pageView = this.synchronizationProposal.data ?
-        this.renderSongComparisonView(this.synchronizationProposal)
-        :
-        this.renderFormView()
 
-    return pageView
+  renderWaitingView() {
+    return html`
+      <waiting-view></waiting-view>
+    `
+  }
+
+  override render() {
+    let pageContent: unknown;
+    switch (this.currentPage) {
+      case "form":
+        pageContent = this.renderFormView();
+        break;
+      case "comparison":
+        pageContent = this.renderSongComparisonView(this.synchronizationProposal);
+        break;
+      case "results":
+        pageContent = html`
+          <failed-uploads-view
+            .failedSongs=${this.failedSongs}
+            @restart=${this.handleRestart}
+          ></failed-uploads-view>
+        `;
+        break;
+      default:
+        pageContent = html`<p>Unknown page</p>`;
+    }
+    return html`
+      ${this.isLoading ? this.renderWaitingView() : html``}
+      <p>${this.errorMessage}</p>
+      ${!this.isLoading ? pageContent : html``}
+    `;
   }
 
   async handleFormSubmission(event: CustomEvent) {
     const payload = event.detail;
-    this.synchronizationProposal = await getSynchronizationProposal(
-        payload.sourceService,
-        payload.targetService,
-        payload.sourceUser,
-        payload.targetUser,
-        payload.playlistName
-    );
+    this.isLoading = true;
+    try {
+      this.synchronizationProposal = await getSynchronizationProposal(
+          payload.sourceService,
+          payload.targetService,
+          payload.sourceUser,
+          payload.targetUser,
+          payload.playlistName
+      );
+      this.requestDetails = this.synchronizationProposal.data.requestDetails;
+      this.proposedChangesId = this.synchronizationProposal.proposedChangesId;
+      this.currentPage = "comparison";
+    } catch (err: any) {
+      this.errorMessage = err.message;
+    }
+    this.isLoading = false;
   }
 
   // TODO: Remove this
   async test() {
     this.synchronizationProposal = await getSynchronizationProposal(
-        "youtube",
         "spotify",
+        "youtube",
         "justin",
         "justin",
         "test",
-        "f528ac1b-ff52-48c5-967e-9ce86db06a34"
+        "test"
     );
+    this.requestDetails = this.synchronizationProposal.data.requestDetails;
+    this.proposedChangesId = this.synchronizationProposal.proposedChangesId;
+    this.currentPage = "comparison";
   }
 
-  async handleRemoveSongMapping(event: CustomEvent) {
-    const { sourceSongId, targetSongId } = event.detail;
-    await updateSynchronizationProposal(
-        this.synchronizationProposal.data.requestDetails.sourceService,
-        this.synchronizationProposal.data.requestDetails.targetService,
-        this.synchronizationProposal.data.requestDetails.sourceUser,
-        this.synchronizationProposal.data.requestDetails.targetUser,
-        this.synchronizationProposal.data.requestDetails.playlist,
-        this.synchronizationProposal.proposedChangesId,
-        "remove",
-        sourceSongId,
-        targetSongId
-    )
-
-    await this.refreshProposal();
-  }
-
-  async handleReplaceSong(event: CustomEvent) {
-    const {sourceSongId, targetSongId} = event.detail;
-    await updateSynchronizationProposal(
-        this.synchronizationProposal.data.requestDetails.sourceService,
-        this.synchronizationProposal.data.requestDetails.targetService,
-        this.synchronizationProposal.data.requestDetails.sourceUser,
-        this.synchronizationProposal.data.requestDetails.targetUser,
-        this.synchronizationProposal.data.requestDetails.playlist,
-        this.synchronizationProposal.proposedChangesId,
-        "update",
-        sourceSongId,
-        targetSongId
-    )
-
-    await this.refreshProposal();
-  }
+  async handleProposalUpdate(event: CustomEvent) {
+    const {proposedChanges} = event.detail;
+    this.synchronizationProposal = {
+      data: proposedChanges,
+      proposedChangesId: this.synchronizationProposal.proposedChangesId
+    }
+  };
 
   async refreshProposal() {
-    this.synchronizationProposal = await getSynchronizationProposal(
-        this.synchronizationProposal.data.requestDetails.sourceService,
-        this.synchronizationProposal.data.requestDetails.targetService,
-        this.synchronizationProposal.data.requestDetails.sourceUser,
-        this.synchronizationProposal.data.requestDetails.targetUser,
-        this.synchronizationProposal.data.requestDetails.playlist,
-        this.synchronizationProposal.proposedChangesId
-    );
+    this.isLoading = true;
+
+    try {
+      this.synchronizationProposal = await getSynchronizationProposal(
+          this.synchronizationProposal.data.requestDetails.sourceService,
+          this.synchronizationProposal.data.requestDetails.targetService,
+          this.synchronizationProposal.data.requestDetails.sourceUser,
+          this.synchronizationProposal.data.requestDetails.targetUser,
+          this.synchronizationProposal.data.requestDetails.playlist,
+          this.synchronizationProposal.proposedChangesId
+      );
+      this.requestDetails = this.synchronizationProposal.data.requestDetails;
+    } catch (err) {
+      console.log(err);
+    }
+    this.isLoading = false;
   }
 
   async synchronizePlaylist() {
-    await synchronizePlaylist(
-        this.synchronizationProposal.data.requestDetails.sourceService,
-        this.synchronizationProposal.data.requestDetails.targetService,
-        this.synchronizationProposal.data.requestDetails.sourceUser,
-        this.synchronizationProposal.data.requestDetails.targetUser,
-        this.synchronizationProposal.data.requestDetails.playlist,
-        this.synchronizationProposal.proposedChangesId
-    )
-
+    this.isLoading = true;
+    try {
+      console.log("Synchronizing playlist...");
+      this.failedSongs = await synchronizePlaylist(
+          this.synchronizationProposal.data.requestDetails.sourceService,
+          this.synchronizationProposal.data.requestDetails.targetService,
+          this.synchronizationProposal.data.requestDetails.sourceUser,
+          this.synchronizationProposal.data.requestDetails.targetUser,
+          this.synchronizationProposal.data.requestDetails.playlist,
+          this.synchronizationProposal.proposedChangesId
+      );
+      this.requestDetails = this.synchronizationProposal.data.requestDetails;
+      this.currentPage = "results";
+    } catch (err) {
+      console.log(err);
+    }
     this.synchronizationProposal = {} as SynchronizationProposal;
+    this.isLoading = false;
+  }
+
+  handleRestart() {
+    this.failedSongs = [];
+    this.synchronizationProposal = {} as SynchronizationProposal;
+    this.errorMessage = "";
+    this.isLoading = false;
+    this.currentPage = "form";
   }
 }
 
