6eb57e65257e3997a099e6dbfd137a74dcbc08a4
Justin Stendara <Justin.stendara@coxautoinc.com>
2025-09-29 14:38:14 -0500

    Update controller

--- music-synchronizer/backend/src/Model/Controller.ts
+++ music-synchronizer/backend/src/Model/Controller.ts
@@ -1,12 +1,18 @@
 import {Song} from "./MusicService.js";
 
 export type ProposedChanges = {
-    confidentProposedChanges: {
-        sourceSong: Song,
-        targetSong: Song
-    }[],
-    uncertainProposedChanges: {
-        sourceSong: Song,
-        targetSong: Song
-    }[]
+    confidentProposedChanges: SongMapping[],
+    uncertainProposedChanges: SongMapping[]
+}
+
+export type SongMapping = {
+    sourceSong: Song,
+    targetSong: Song
+}
+
+export type SynchronizeMusicSources = {
+    [key: string]: {
+        failedSongs: Song[],
+        unconfidentSongs: SongMapping[]
+    }
 }
