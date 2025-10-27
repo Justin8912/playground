0430c37dd87c6a89d9cadfbd024b27b79c233588
justin8912 <jnstendara@gmail.com>
2025-10-26 19:42:16 -0500

    Stopping point

--- codenames/UI/codegen.ts
+++ codenames/UI/codegen.ts
@@ -3,8 +3,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "../backend/bff/src/schema.graphql",
-  documents: "src/**/*.tsx",
+  schema: "http://localhost:4000/",
+  // documents: "src/**/*.tsx",
   generates: {
     "src/gql/": {
       preset: "client",
