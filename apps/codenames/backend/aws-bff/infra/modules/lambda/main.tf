f94ba22bd1bef40333a5267aa579c552b076f1a0
Justin Stendara <Justin.stendara@coxautoinc.com>
2025-12-17 14:43:21 -0600

    update dummy file path

--- apps/codenames/backend/aws-bff/infra/modules/lambda/main.tf
+++ apps/codenames/backend/aws-bff/infra/modules/lambda/main.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "deck_generator" {
     role          = aws_iam_role.lambda_role.arn
     handler       = "app.handler"
     runtime       = "python3.12"
-    filename      = "${path.module}/../modules/lambda/resources/dummy.zip"
+    filename      = "${path.module}/resources/dummy.zip"
     timeout       = 10
     memory_size   = 512
 
